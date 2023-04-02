088436d4a5836f0a12b08bb84f61b59feae505e7
Austin Dahl <flyboy288@gmail.com>
2023-04-01 19:31:51 -0500

    Added a bit more for the gitignore and more c++
    No need to have some files in the git repo, also prepped and studied a bit for adding doxygen for documentation.

---
+++ Beginner/Basic Input Output/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* argv[], char* envp[]) {
+	std::string name;
+	//typically used for when you would create a string object with value already assigned
+	std::string str("Austin"); 
+	std::cout << "Please enter your name: " << std::endl;
+	std::cin >> name;
+	std::cout << "Thank you " << name << ", hope you are having a nice day" << std::endl;
+	std::cout << "We can also get your name with a getline(cin, variable), \n"
+			  << "but have to use cin.ignore to clear the whitespace left from previous cin, \n"
+			  << "please enter a new name: " << std::flush;
+	std::cin.ignore();
+	std::getline(std::cin, name);
+	std::cout << "The name is now " + name << std::endl;
+	return 0;
+}
