5e45994061ebfef2a8c8e5534d8bac22e1705fdb
Austin Dahl <flyboy288@gmail.com>
2023-04-12 17:08:25 -0500

    Pushing previous changes

--- Beginner/Basic Input Output/main.cpp
+++ Beginner/Basic Input Output/main.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
+#include <fstream>
 #include <string>
-
+// cin leaves a whitespace char, so have to use cin.ignore to clear the whitespace left over
 int main(int argc, char* argv[], char* envp[]) {
-	std::string name;
-	//typically used for when you would create a string object with value already assigned
-	std::string name1("Austin"); 
-	std::cout << "Please enter your name: " << std::endl;
-	std::cin >> name;
-	std::cout << "Thank you " << name << ", hope you are having a nice day" << std::endl;
-	std::cout << "We can also get your name with a getline(cin, variable), \n"
-			  << "but have to use cin.ignore to clear the whitespace left from previous cin, \n"
-			  << "please enter a new name: " << std::flush;
-	std::cin.ignore();
-	std::getline(std::cin, name);
-	std::cout << "The name is now " + name << std::endl;
+	std::string myText;
+	std::ifstream testFile("test.txt");
+	if (testFile.is_open()) {
+		while (std::getline(testFile, myText))
+		{
+			std::cout << myText << std::endl;
+		}
+	}
+	else {
+		std::cout << "Error opening file." << std::endl;
+	}
+
 	return 0;
 }
